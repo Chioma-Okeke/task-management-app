3706d29c93c0f91ce80c7a33e0a4677ee297ecf2
Okeke Chioma Princess <okekegeraldenne@gmail.com>
2025-03-31 20:18:03 +0100

    added fnal api routes

--- controller/taskController.js
+++ controller/taskController.js
@@ -2,9 +2,23 @@ const moment = require("moment");
 const taskModel = require("../model/taskModel");
 const userModel = require("../model/userModel");
 
+const get_task_by_id = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const task = await taskModel.findById(id);
+        if (!task) {
+            return res.status(404).json({ message: "Task not found." });
+        }
+
+        res.status(200).json({ message: "Task successfully fetched.", task });
+    } catch (error) {
+        next(error);
+    }
+};
+
 const get_user_tasks = async (req, res, next) => {
     const userId = req.user._id;
-    const { categories, deadline } = req.query;
+    const { categories, deadline, status } = req.query;
 
     try {
         let filter = { userId };
@@ -13,10 +27,19 @@ const get_user_tasks = async (req, res, next) => {
             filter.categories = categories;
         }
 
-        console.log(moment(req.body.deadline, "DD/MM/YYYY").toISOString(), "deadline date")
-
         if (deadline) {
-            filter.deadline = { $lte: moment(req.body.deadline, "DD/MM/YYYY").toISOString() };
+            filter.deadline = {
+                $lte: moment(deadline, "DD/MM/YYYY").toISOString(),
+            };
+        }
+
+        if (status) {
+            if (status !== "true" && status !== "false") {
+                return res.status(400).json({
+                    message: "Invalid status value. Must be 'true' or 'false'.",
+                });
+            }
+            filter.completed = status === "true";
         }
 
         const tasks = await taskModel.find(filter);
@@ -36,17 +59,14 @@ const create_a_task = async (req, res, next) => {
 
     try {
         const user = await userModel.findById(userId);
-        // if (!user) {
-        //     return res
-        //         .status(401)
-        //         .json({ msg: "You need to be logged in to create a post" });
-        // }
 
         const newTask = new taskModel({
             ...req.body,
             userId: userId,
             categories: req.body.categories.toLowerCase(),
-            deadline: moment(req.body.deadline, "DD/MM/YYYY").add(1, 'days').toISOString()
+            deadline: moment(req.body.deadline, "DD/MM/YYYY")
+                .add(1, "days")
+                .toISOString(),
         });
         const createdTask = await newTask.save();
         user.taskIds.push(createdTask._id);
@@ -61,16 +81,22 @@ const create_a_task = async (req, res, next) => {
 };
 
 const update_a_task = async (req, res, next) => {
+    const userInfo = req.user
     const updates = req.body;
     const { id } = req.params;
 
+    if (!userInfo.taskIds.includes(id)) {
+        return res
+            .status(401)
+            .json({ msg: "You can only edit tasks created by you" });
+    }
     try {
         const taskUpdated = await taskModel.findByIdAndUpdate(id, updates, {
             new: true,
             runValidators: true,
         });
         if (!taskUpdated) {
-            return res.status(404).json({ msg: "No post found" });
+            return res.status(404).json({ msg: "No task found" });
         }
         res.status(200).json(taskUpdated);
     } catch (error) {
@@ -91,14 +117,14 @@ const delete_a_task = async (req, res, next) => {
 
         if (task.userId.toString() !== userId.toString()) {
             return res.status(403).json({
-                msg: "Unauthorized: Only authors can delete this post",
+                message: "You can only delete task created by you.",
             });
         }
 
         await taskModel.findByIdAndDelete(id);
 
         if (user) {
-            user.taskIds = user.taskIds.filter((taskId) => taskId !== id);
+            user.taskIds = user.taskIds.filter((taskId) => taskId.toString() !== id.toString());
             await user.save();
         }
         res.status(200).json({ msg: "Task successfully deleted" });
@@ -107,9 +133,44 @@ const delete_a_task = async (req, res, next) => {
     }
 };
 
+const task_status_update = async (req, res, next) => {
+    const userInfo = req.user;
+    const { id } = req.params;
+    const { status } = req.body;
+
+    if (!userInfo.taskIds.includes(id)) {
+        return res
+            .status(401)
+            .json({ msg: "You can only edit tasks created by you" });
+    }
+
+    try {
+        if (typeof status !== "boolean") {
+            return res
+                .status(400)
+                .json({
+                    message: "Invalid status value. Must be true or false.",
+                });
+        }
+        const task = await taskModel.findOne({ _id: id, userId: userInfo._id });
+        if (!task) {
+            return res.status(404).json({ message: "Task not found" });
+        }
+
+        task.completed = status;
+        await task.save();
+
+        res.status(200).json({ message: "Task status updated.", task });
+    } catch (error) {
+        next(error);
+    }
+};
+
 module.exports = {
+    get_task_by_id,
     get_user_tasks,
     create_a_task,
     update_a_task,
     delete_a_task,
+    task_status_update,
 };
--- router/taskRoute.js
+++ router/taskRoute.js
@@ -5,12 +5,16 @@ const {
     create_a_task,
     update_a_task,
     delete_a_task,
+    get_task_by_id,
+    task_status_update,
 } = require("../controller/taskController");
 
 const taskRouter = Router()
+    .get("/tasks/:id", authMiddleware, get_task_by_id)
     .get("/tasks", authMiddleware, get_user_tasks)
     .post("/task", authMiddleware, create_a_task)
     .put("/tasks/:id", authMiddleware, update_a_task)
+    .put("/tasks/:id/status", authMiddleware, task_status_update)
     .delete("/tasks/:id", authMiddleware, delete_a_task);
 
 module.exports = taskRouter;
